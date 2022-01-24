8231b6e281ff1849643979d8aab6344d868f2a54
User <dammiaazmi62@gmail.com>
2022-01-24 15:39:11 +0800

    student servlet

--- src/main/java/com/example/skbtsystem/StudentRegistrationServlet.java
+++ src/main/java/com/example/skbtsystem/StudentRegistrationServlet.java
@@ -8,7 +8,9 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 
 @WebServlet(name = "StudentRegistrationServlet", value = "/StudentRegistrationServlet")
@@ -23,14 +25,49 @@ public class StudentRegistrationServlet extends HttpServlet
         String studentPhone = request.getParameter("studentPhone");
         String studentEmail = request.getParameter("studentEmail");
         String studentPass = request.getParameter("studentPass");
+        RequestDispatcher dispatcher = null;
+        Connection conn = null;
 
-        PrintWriter out = response.getWriter();
-        out.print(studentName);
-        out.print(studentPhone);
-        out.print(studentEmail);
-        out.print(studentPass);
+        try
+        {
+            Class.forName("org.postgresql.Driver");
+            String dbURL = "postgres://wzhkegxdhdsbgm:2de0ec5650e40e6383f4ad61c98e44dec650a6a8f9d79fdf03efa59408d53f99@ec2-3-212-143-188.compute-1.amazonaws.com:5432/d9pq1r2tte9jfs"; //ni url dri heroku database
+            String user = "wzhkegxdhdsbgm"; //ni user dri heroku database
+            String pass = "2de0ec5650e40e6383f4ad61c98e44dec650a6a8f9d79fdf03efa59408d53f99"; //ni password dri heroku database
+            conn = DriverManager.getConnection(dbURL, user, pass);
+
+            PreparedStatement pst = conn.prepareStatement("insert into student(studentname, studentPass, studentEmail, studentPhone) values (?,?,?,?)");
+            pst.setString(1,studentName);
+            pst.setString(2,studentPhone);
+            pst.setString(3,studentEmail);
+            pst.setString(4,studentPass);
+
+            int rowCount = pst.executeUpdate();
+            dispatcher = request.getRequestDispatcher("StudentRegister.jsp");
+
+            if (rowCount >0)
+            {
+                request.setAttribute("status", "success");
 
 
+            }else{
+                request.setAttribute("status", "failed");
+            }
+            dispatcher.forward(request, response);
+
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+
+
+}finally{
+        try{
+            conn.close();
+
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
 }
-}
+}}
 
