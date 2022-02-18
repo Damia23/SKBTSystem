662f180096e795e682f9a827ca3a5cbc7cbe1bab
User <dammiaazmi62@gmail.com>
2022-02-18 21:47:52 +0800

    login & registration

--- src/main/java/com/example/skbtsystem/TeacherRegistrationServlet.java
+++ src/main/java/com/example/skbtsystem/TeacherRegistrationServlet.java
@@ -33,7 +33,7 @@ public class TeacherRegistrationServlet extends HttpServlet {
             PreparedStatement st;
             String query="insert into library_user(userid,username,userpass," +
                     "useremail,userphone,accountid) " +
-                    "values(nextval('library_user_userid_seq'),?,?,?,?,20)";
+                    "values(nextval('userid_req'),?,?,?,?,20)";
             st = conn.prepareStatement(query);
             st.setString(1,tcName);
             st.setString(2,tcPass);
